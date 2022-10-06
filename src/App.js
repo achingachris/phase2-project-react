afdf767cdaf6e988c8cca7e222b5cfde93705807
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-06 14:49:35 +0300

    format file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import { Container } from "react-bootstrap";
-import { Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+import { Routes, Route } from 'react-router-dom'
 
 // Layout
-import Layout from "./layout/Layout";
+import Layout from './layout/Layout'
 
 // pages
-import Home from "./pages/Home";
-import AddPlane from "./pages/AddPlane";
-import NotFound from "./pages/NotFound";
+import Home from './pages/Home'
+import AddPlane from './pages/AddPlane'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
     <Layout>
       <Container>
         <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/add" element={<AddPlane />} />
+          <Route path='/' element={<Home />} exact />
+          <Route path='/add' element={<AddPlane />} />
           <Route path='*' element={<NotFound />} />
         </Routes>
       </Container>
     </Layout>
-  );
-};
+  )
+}
 
-export default App;
+export default App

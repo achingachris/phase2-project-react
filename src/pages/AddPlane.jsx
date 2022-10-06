157090b88127a319b5e778a0e8e4d9d09c2677fd
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-06 14:53:56 +0300

    clean code

--- src/pages/AddPlane.jsx
+++ src/pages/AddPlane.jsx
@@ -1,19 +1,19 @@
-import Header from "../components/Header";
-import Meta from "../components/Meta";
-import Form from "../components/addPlane/Form";
+import Header from '../components/Header'
+import Meta from '../components/Meta'
+import Form from '../components/addPlane/Form'
 
 const AddPlane = () => {
-	// page content
-	const pageTitle = "Add Plane Spotted";
-	const pageDescription = "Add a Plane Spotted by filling out this simple Form";
+  // page content
+  const pageTitle = 'Add Plane Spotted'
+  const pageDescription = 'Add a Plane Spotted by filling out this simple Form'
 
-	return (
-		<div>
-			<Meta title={pageTitle} />
-			<Header head={pageTitle} description={pageDescription} />
-			<Form />
-		</div>
-	);
-};
+  return (
+    <div>
+      <Meta title={pageTitle} />
+      <Header head={pageTitle} description={pageDescription} />
+      <Form />
+    </div>
+  )
+}
 
-export default AddPlane;
+export default AddPlane

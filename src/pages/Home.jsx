3d31a99bd61ae2f74efc54a719f55e84563922c9
itschrisagain <chris.achinga@student.moringaschool.com>
2022-10-07 14:23:09 +0300

    remove search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import Header from '../components/Header'
 import Meta from '../components/Meta'
 import Planes from '../components/homepage/Planes'
-import Search from '../components/homepage/Search'
 
 const Home = () => {
+
+
   // page content
   const pageTitle = 'Plane Spotter'
   const pageDescription = 'One Stop for all your plane spotting Geeky needs'
@@ -12,7 +13,6 @@ const Home = () => {
     <div>
       <Meta title={pageTitle} />
       <Header head={pageTitle} description={pageDescription} />
-      <Search />
       <Planes />
     </div>
   )
